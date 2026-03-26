05c385da27ac9c765c2bea1ca3373a17ab2928aa
Raphael_Doizi <doizi.raphael@gmail.com>
2026-03-26 11:59:36 +0100

    ✈️ translate EN to FR error messages

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+'reset' => 'Si vous avez un compte utilisateur, votre mot de passe a été réinitialisé.',
+'sent' => 'Nous avons envoyé par e-mail votre lien de réinitialisation de mot de passe.',
+'throttled' => 'Veuillez patienter avant de réessayer.',
+'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+'user' => "Si vous avez un compte utilisateur, votre mot de passe a été réinitialisé.",
+
+];
